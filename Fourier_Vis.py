75a3d2c755d6161d49b114007d2f832813f0a642
erikedwards <erik.p.edwards@gmail.com>
2019-07-04 23:34:53 -0700

    2nd commit. Mostly works. TODO:  - Add input for "n" value (num values of series to add.)  - Add scaling inputs (change wave x scale)

--- Fourier_Vis.py
+++ Fourier_Vis.py
@@ -5,9 +5,6 @@ import math
 # CONSTANTS
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 700
-circX = int(SCREEN_WIDTH / 4)
-circY = int(SCREEN_HEIGHT / 2)
-rad = 200
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -26,8 +23,15 @@ FONT = pygame.font.SysFont('Courier New', 10)
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
+# init game values
+circX = int(SCREEN_WIDTH / 4)
+circY = int(SCREEN_HEIGHT / 2)
+circScale = 150
+wave = []
+waveX = int(SCREEN_WIDTH * (2/4))
+# waveY = int(SCREEN_HEIGHT / 2)
 time = 0.00
-timeStep = 0.01
+timeStep = 0.005
 
 # Main Loop
 run = True
@@ -37,21 +41,44 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    # --- Game logic should go here
-    time += timeStep
-    x = rad * math.cos(time)
-    y = -1 * rad * math.sin(time)
-
-
-    # --- Screen-clearing code goes here
+    # Clear Screen
     screen.fill(WHITE)
 
-    # --- Drawing code should go here
-    pygame.draw.circle(screen, BLACK, [circX, circY], rad, 1)
-    pygame.draw.line(screen, GREEN, [circX, circY], [circX + x, circY + y])
+    # Game logic
+    time += timeStep
+
+    # --- draw wave CS
+    rad = int(circScale * (4 / (1 * math.pi)))
+    pygame.draw.line(screen, BLACK, [waveX, circY - rad], [waveX, circY + rad])
+    pygame.draw.line(screen, BLACK, [waveX - 50, circY], [SCREEN_WIDTH, circY])
+
+    x = circX
+    y = circY
+    for n in range(1, 13, 2):
+        prevX = x
+        prevY = y
+        rad = int(circScale * ( 4 / (n * math.pi)))
+        x += int(rad * math.cos(n * time))
+        y += int(-1 * rad * math.sin(n * time))
+        # --- draw circle
+        pygame.draw.circle(screen, BLACK, [prevX, prevY], rad, 1)
+        pygame.draw.line(screen, GREEN, [prevX, prevY], [x, y])
+
+    # Add y value of smallest circle to wave
+    wave.insert(0, y)
+    if len(wave) > 500:
+        wave.pop(-1)
+
+    # --- draw wave
+    for i in range(0, len(wave)):
+        pX = int(waveX + i)
+        pY = int(wave[i])
+        pygame.draw.circle(screen, GREEN, [pX, pY], 1)
+    # --- draw connecting level line
+    pygame.draw.line(screen, GREY, [x, y], [waveX, y])
 
 
-    # --- Go ahead and update the screen with what we've drawn.
+    # --- Update the screen
     pygame.display.flip()
 
     # --- Limit to 60 frames per second
