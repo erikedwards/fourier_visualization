9d34825b070ba69c8c745e6104a892f1cd82dba6
erikedwards <erik.p.edwards@gmail.com>
2025-08-24 13:23:56 -0700

    Update fourier.js

--- fourier.js
+++ fourier.js
@@ -1,6 +1,8 @@
 let screenWidth = 1200;
 let screenHeight = 600;
-let wave = [];
+let waves = [];
+let colors = [];
+// let wave = [];
 let time;
 let maxN;
 let slider;
@@ -19,6 +21,11 @@ function setup() {
   
   maxN = 11;
   
+  for (i=0; i<100; i++) {
+    waves[i] = [];
+    colors[i] = color(random(255), random(255), random (255));
+  }
+  
   // Create a slider and place it at the top of the canvas.
   slider = createSlider(1, 50);
   slider.position(10, height - 50);
@@ -29,14 +36,15 @@ function setup() {
 
 function draw() {
   background(220);
+  stroke(0,0,0,255);
   maxN = slider.value();
   
   time += timeStep;
   
   text("Time = " + time, 10, 30);
   text("maxN = " + maxN, 10, 50);
-  text("wave[] = " + wave, 10, 70);
-  text("wave.length = " + wave.length, 10, 90);
+  // text("wave[] = " + wave, 10, 70);
+  // text("wave.length = " + wave.length, 10, 90);
   
   // draw wave CS
   rad = circScale * (4 / (1 * PI));
@@ -54,22 +62,43 @@ function draw() {
     y += -1 * rad * sin(n*time)/2;
     circle(prevX, prevY, rad);
     line(prevX, prevY, x, y);
+    let w = (n+1) / 2;
+    waves[w].splice(0,0,y);
+    if ( waves[w].length > screenWidth) {
+      waves[w].splice(waves[w].length - 1, 1);
+    }
   }
   //circle(x, y, rad/PI);
   
+  for (u = 0; u < waves.length; u++) {
+    for (j = 0; j < waves[u].length; j++) {
+      pX = waveX + j;
+      pY = waves[u][j];
+      // set alpha depending on u value. 
+      // set alpha relative to depth in array
+      alph = ((waves.length/u)*1);
+      
+      // stroke(0,0,0,alph);
+      stroke(colors[u], 100);
+      circle(pX, pY, 1);
+    }
+  }
+  
+  
+  
   // add y location of smallest circle to the wave
-  wave.splice(0, 0, y);
-  if (wave.length > 650) {
-    let d = wave.length;
-    wave.splice(d-1, 1);
-  }
+  // wave.splice(0, 0, y);
+  // if (wave.length > 650) {
+  //   let d = wave.length;
+  //   wave.splice(d-1, 1);
+  // }
   
-  for (let i = 0; i < wave.length; i++) {
-    pX = waveX + i;
-    pY = wave[i];
-    circle(pX, pY, 1);
-  }
-  
+//  for (let i = 0; i < wave.length; i++) {
+//    pX = waveX + i;
+//    pY = wave[i];
+//    circle(pX, pY, 1);
+//  }
+  stroke(0,0,0,255);
   line(x, y, waveX, y);
   
   
@@ -77,10 +106,3 @@ function draw() {
   
   
 }
-
-
-
-
-
-
-
